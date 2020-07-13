09668873b852342d63c4ee261cc0c1c90f07712f
Odritax <pabloyanezbarriga@gmail.com>
2020-07-13 14:06:03 -0400

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Listas titulo="Habilidades en Progreso" v-bind:enlaces="Habilidades_en_progreso"/>
+    <Listas titulo="Mis Habilidaes" v-bind:enlaces="Habilidades"/>
+  </div>
+</template>
+<script>
+  import Listas from './components/Listas.vue'
+  export default {
+    name: 'App',
+    components: {
+      Listas
+    },
+    data: () => {
+      return {
+        Habilidades_en_progreso:[
+          {texto:"CSS",url:"http://www.google.cl/"},
+          {texto:"Java Script 6",url:"http://www.youtube.cl/"},
+          {texto:"VUE",url:"http://www.behance.com/"},
+          {texto:"Python",url:"http://www.twitter.cl/"}
+        ],
+        Habilidades:[
+          {texto:"Cocinar",url:"www.youtube.com"},
+          {texto:"Comer",url:"www.instagram.com"},
+          {texto:"Limpiar",url:"www.youtube.com"}
+        ]
+      };
+    }
+  };
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
